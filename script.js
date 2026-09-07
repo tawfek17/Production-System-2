c74a8dc75d614afc7e7d52fbad9156665ec20e46
tawfek17 <68343377+tawfek17@users.noreply.github.com>
2026-09-07 11:00:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const STORAGE_KEY = "production_system_v6";
 const FIREBASE_PATH = "productionSystem";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,8 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 
 const defaultState = {
